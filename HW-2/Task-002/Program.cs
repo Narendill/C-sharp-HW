9b7fb199c578127f945c4880ff949c4ad2f72c44
nikolay_matev <matev_n@mail.ru>
2022-11-23 21:17:53 +0200

    Added the 2nd HW

---
+++ HW-2/Task-002/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6 */
+
+using static System.Console;
+Clear();
+
+WriteLine("Input a number:");
+string number = ReadLine();
+
+if (number.Length < 3)
+{
+    WriteLine("There is no the 3rd number.");
+}
+else
+{
+    WriteLine(number[2]);
+}
---
+++ HW-2/Task-003/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */
+
+using static System.Console;
+Clear();
+
+WriteLine("Input a number:");
+int number = int.Parse(ReadLine());
+
+if(number > 7 || number < 1)
+{
+    WriteLine("Your input isn't correct.");
+}
+else if(number == 6 || number == 7)
+{
+    WriteLine("Yes, it's a day off.");
+}
+else
+{
+    WriteLine("No, it isn't a day off.");
+}
