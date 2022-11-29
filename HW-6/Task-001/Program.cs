482a70704ea57fd23f45f65f47aa915c23238f21
nikolay_matev <matev_n@mail.ru>
2022-11-29 18:07:18 +0200

    Added the 6th HW

---
+++ HW-6/Task-001/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+
+using static System.Console;
+Clear();
+
+int InputNumber(string message)
+{
+    WriteLine(message);
+    int M = int.Parse(ReadLine());
+    return M;
+}
+
+void MoreThanZero(int quantity)
+{
+    int count = 0;
+    for (int i = 0; i < quantity; i++)
+    {
+        WriteLine($"Input number #{i + 1}: ");
+        int input_number = int.Parse(ReadLine());
+        if (input_number > 0) count++;
+    }
+    WriteLine($"There are {count} numbers more than 0.");
+}
+
+
+
+int M = InputNumber("How many numbers do you want to input? ");
+MoreThanZero(M);
---
+++ HW-6/Task-002/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+using static System.Console;
+Clear();
+
+double InputNumber(string message)
+{
+    WriteLine(message);
+    double M = double.Parse(ReadLine());
+    return M;
+}
+
+void Intersection(double b1, double k1, double b2, double k2)
+{
+    if (k1 == k2 && b1 == b2) WriteLine("Lines on the plane coincide.");
+    else if (k1 == k2 && b1 != b2) WriteLine("Lines on a plane are parallel. There are no intersection points.");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        WriteLine($"The point of intersection: [{x};{y}]");
+    }
+
+}
+
+double b1 = InputNumber("Input b1: ");
+double k1 = InputNumber("Input k1: ");
+double b2 = InputNumber("Input b2: ");
+double k2 = InputNumber("Input k2: ");
+
+Intersection(b1, k1, b2, k2);
